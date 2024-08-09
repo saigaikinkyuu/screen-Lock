800f2661a8514ce8ae367151199f2ab5c2a927c9
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-08-09 16:39:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var number_pass = ""
 function checkFullScreen() {
   if (document.fullscreenElement) {
     iconChange(false)
@@ -22,7 +23,28 @@ checkFullScreen()
 
 document.addEventListener('keydown', function(event) {
   // 例: F5キーの更新を無効化
-  if (event.keyCode === 115) {
+  if (event.keyCode === 115 || event.keyCode === 17) {
     event.preventDefault();
   }
+  if (event.keyCode === 27) {
+    document.exitFullscreen();
+  }
+  if(event.keyCode <= 48 && event.keyCode >= 57){
+    number_pass += event.keyCode-48
+    if(number_pass.length === 6){
+      $.getJSON("https://script.google.com/macros/s/AKfycbxOFLC9LnmR34UiRXRx8bgOd8XGF1xVBT0uSA6J0DqwEtBbwf0enPj2BUbwWoyTO-Kl/exec?k=check" + number_pass, function (data) {
+        if(data.sattus === "Success"){
+          document.exitFullscreen();
+        }
+      })
+    }
+  }
 });
+
+function setPass(){
+  $.getJSON("https://script.google.com/macros/s/AKfycbxOFLC9LnmR34UiRXRx8bgOd8XGF1xVBT0uSA6J0DqwEtBbwf0enPj2BUbwWoyTO-Kl/exec?k=check" + number_pass, function (data) {
+    if(data.sattus === "Success"){
+      console.log("ワンタイム・パスコードを発行しました。")
+    }
+  })
+}
